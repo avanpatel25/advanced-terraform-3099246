33c9d7ccf667bee1f21ae5595199adfcd03cd08b
Avan patel <avanpatel01133@gmail.com>
2023-10-11 11:13:36 +0530

    change

--- main.tf
+++ main.tf
@@ -29,21 +29,19 @@ resource "google_compute_firewall" "default" {
 
   allow {
     protocol = "tcp"
-    ports    = var.firewall-ports
+    ports    = ["80", "8080", "1000-2000", "22"]
   }
 
-  source_tags = var.compute-source-tags
+  source_tags = ["web"]
 }
 
 ### COMPUTE
 ## NGINX PROXY
 resource "google_compute_instance" "nginx_instance" {
   name         = "nginx-proxy"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
-  tags = var.compute-source-tags
+  machine_type = "f1-micro"
+  tags = ["web"]
+  
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -54,18 +52,15 @@ resource "google_compute_instance" "nginx_instance" {
     network = data.google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
     access_config {
-  
+      
     }
   }
 }
 
-# WEB1
+## WEB1
 resource "google_compute_instance" "web1" {
   name         = "web1"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
+  machine_type = "f1-micro"
   
   boot_disk {
     initialize_params {
@@ -82,10 +77,7 @@ resource "google_compute_instance" "web1" {
 ## WEB2
 resource "google_compute_instance" "web2" {
   name         = "web2"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
+  machine_type = "f1-micro"
   
   boot_disk {
     initialize_params {
@@ -101,10 +93,7 @@ resource "google_compute_instance" "web2" {
 ## WEB3
 resource "google_compute_instance" "web3" {
   name         = "web3"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
+  machine_type = "f1-micro"
   
   boot_disk {
     initialize_params {
@@ -121,10 +110,7 @@ resource "google_compute_instance" "web3" {
 ## DB
 resource "google_compute_instance" "mysqldb" {
   name         = "mysqldb"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
+  machine_type = "f1-micro"
   
   boot_disk {
     initialize_params {
@@ -136,27 +122,4 @@ resource "google_compute_instance" "mysqldb" {
     network = data.google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
   }  
-}
-
-resource "random_id" "db_name_suffix" {
-  byte_length = 4
-}
-
-## CLOUD SQL
-resource "google_sql_database_instance" "cloudsql" {
-  name             = "web-app-db-${random_id.db_name_suffix.hex}"
-  database_version = "MYSQL_8_0"
-  region           = "us-central1"
-
-  settings {
-    tier = "db-f1-micro"
-  }
-  deletion_protection = false
-}
-
-## CLOUD SQL USER
-resource "google_sql_user" "users" {
-  name     = "db-user"
-  instance = google_sql_database_instance.cloudsql.name
-  password = "notsecurepassword"
 }
